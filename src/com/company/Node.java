775e25adfb9aa2fece2ae0e573911fced0c54768
Flame Thrower <flame.thrower@mail.ru>
2017-10-21 20:27:13 +0300

    Initial commit

---
+++ src/com/company/Node.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Node implements Entry {
+
+    private Object key;
+    private Object value;
+
+
+    @Override
+    public Object getKey() {
+        return null;
+    }
+
+    @Override
+    public Object getValue() {
+        return null;
+    }
+
+    @Override
+    public Object setValue(Object value) {
+        return null;
+    }
+}
